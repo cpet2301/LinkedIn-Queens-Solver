5439cae1c755a74f3b557110a3437c2c729c6167
Caleb Petterson <clebamail@gmail.com>
2024-12-16 21:23:22 -0600

    Added solver_experiment.py: will serve as a 'main' for conducting our experiments; 3 trials of 10 puzzles with different grid sizes, comparing the time complexity, space complexity, and success rate of each search algorithm

---
+++ solver_experiment.py
@@ -0,0 +1,5 @@
+from screen_capture import create_state_space, display_grid
+from search_algorithms import breadth_first_search, depth_first_search, local_search, backtracking_search, genetic_search
+
+if __name__ == "__main__":
+    pass
